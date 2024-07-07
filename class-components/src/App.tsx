2cbed82667ce71e54b584502d0a24bced8bc7df9
victoria tonkovich <tonkovich.vika@gmail.com>
2024-07-07 18:04:42 +0300

    feat: implement search panel

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,11 +1,15 @@
-
 import "./App.css";
 
 function App() {
-
   return (
     <>
-    
+      <div>
+        <div className="search-panel">
+          <input className="search-input" type="text" placeholder="Search..." />
+          <button className="search-btn">Search</button>
+        </div>
+        <div className="result"></div>
+      </div>
     </>
   );
 }
